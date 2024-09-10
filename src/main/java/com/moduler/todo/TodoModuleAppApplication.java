7293b93a76e15ae50a77ba678a06e9e8334b2b2e
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2024-09-10 18:59:41 -0400

    master commit

---
+++ src/main/java/com/moduler/todo/TodoModuleAppApplication.java
@@ -0,0 +1,13 @@
+package com.moduler.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoModuleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoModuleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moduler/todo/TodoModuleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moduler.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoModuleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
